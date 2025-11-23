2b2cc2c6f38f2e0fa51ddd5db5660f64a830e492
ivan beltran <ivanbeltran1610@gmail.com>
2025-11-22 19:03:24 -0600

    deploy inicial

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-const express=require("express")
-const cors=require("cors")
-const dotenv=require("dotenv")
-const conectarBD=require("./config/database")
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const conectarBD = require("./config/database");
 
-dotenv.config()
-conectarBD()
+dotenv.config();
+conectarBD();
 
-const app= express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use("/api/clientes", require("./routes/Cliente.route"))
-app.use("/api/envios", require("./routes/Envio.route"))
-app.use("/api/rutas",  require("./routes/Ruta.route"))
-app.use("/api/repartidores", require("./routes/Repartidor.route"))
-app.use("/api/pagos", require("./routes/Pago.route"))
+app.use("/api/clientes", require("./routes/Cliente.route"));
+app.use("/api/envios", require("./routes/Envio.route"));
+app.use("/api/rutas", require("./routes/Ruta.route"));
+app.use("/api/repartidores", require("./routes/Repartidor.route"));
+app.use("/api/pagos", require("./routes/Pago.route"));
 
-app.listen(process.env.PORT,()=>{
-    console.log("servidor escuchando")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor escuchando en el puerto " + PORT);
+});
