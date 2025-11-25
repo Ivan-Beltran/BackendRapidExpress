3d54172d0b3c66828f5b36ed159e56433f972f03
ivan beltran <ivanbeltran1610@gmail.com>
2025-11-24 22:08:35 -0600

    arregalndo envios

--- routes/Envio.route.js
+++ routes/Envio.route.js
@@ -5,8 +5,8 @@ const envioCtrl=require('../controllers/Envios.controller')
 
 router.get("/",envioCtrl.obtenerEnvios)
 router.post("/",envioCtrl.crearEnvios)
-router.put("/",envioCtrl.actualizarEnvios)
-router.delete("/",envioCtrl.eliminarEnvios)
+router.put("/:id",envioCtrl.actualizarEnvios)
+router.delete("/:id",envioCtrl.eliminarEnvios)
 
 
 module.exports=router
